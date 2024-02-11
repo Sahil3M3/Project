df5ce31452252d85fc4d04ca86fb562e4e3c0da4
sahil3m3@gmail.com <sahil3m3@gmail.com>
2024-02-11 12:03:26 +0530

    Front-end Static pages

--- Courier Service/courier-frontend/src/App.js
+++ Courier Service/courier-frontend/src/App.js
@@ -1,14 +1,27 @@
 
-import './App.css';
-import AppBar from './components/Appbar';
-import User from './components/user';
+import React from 'react';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import  Home  from './page/Home';
+import About  from './page/About';
+import Contact from './page/Contact';
+import Services   from './page/Services';
+import Pagenotfound from './page/Pagenotfound'
+
 function App() {
   return (
-    <div className="App">
-    <AppBar/>
-    <User/>
+    <div>
+<BrowserRouter>
+<Routes>
+  <Route path="/"element={<Home/>} />
+  <Route path='/about'element={<About/>}/>
+  <Route path='/contact'element={<Contact/>}/>
+  <Route path='/services'element={<Services/>}/>
+  <Route path='*'element={<Pagenotfound/>}/>
+</Routes>
+</BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
