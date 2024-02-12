ed78def01040b69cfcc0c822cbd471c26a9a52a1
sahil3m3@gmail.com <sahil3m3@gmail.com>
2024-02-13 02:22:53 +0530

    Backend-end

--- Courier Service/spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/sunbeam/entity/User.java
+++ Courier Service/spring_boot_backend_template/spring_boot_backend_template/src/main/java/com/app/sunbeam/entity/User.java
@@ -9,14 +9,24 @@ import javax.persistence.GenerationType;
 
 import org.springframework.data.annotation.Id;
 @Entity
-public class User {
+public class User extends BasicEntity {
 
-	    @javax.persistence.Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Integer id;
+	
+	@Column
+	private String email;
+	
+	@Column
+    private int pinCode;
+	
+	    @Column
+	    private String area;
+	
+	    @Column(nullable = false)
+	    private int number;
+	
 
-	    @Column(unique = true, nullable = false)
-	    private String username;
+	    @Column(nullable = false)
+	    private String name;
 
 	    @Column(nullable = false)
 	    private String password;
@@ -26,52 +36,6 @@ public class User {
 	    private UserRole role;
 
 	    
-	    public User()
-	    {
-	    	
-	    }
 	    
 	    
-		public User(Integer id, String username, String password, UserRole role) {
-		
-			this.id = id;
-			this.username = username;
-			this.password = password;
-			this.role = role;
-		}
-
-		public Integer getId() {
-			return id;
-		}
-
-		public void setId(Integer id) {
-			this.id = id;
-		}
-
-		public String getUsername() {
-			return username;
-		}
-
-		public void setUsername(String username) {
-			this.username = username;
-		}
-
-		public String getPassword() {
-			return password;
-		}
-
-		public void setPassword(String password) {
-			this.password = password;
-		}
-
-		public UserRole getRole() {
-			return role;
-		}
-
-		public void setRole(UserRole role) {
-			this.role = role;
-		}
-
-	    
-	
-}
+	   }
